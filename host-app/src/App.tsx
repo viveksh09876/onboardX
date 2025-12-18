516c42d7369e14e92ee271993a1905094135fdd4
Vivek Sharma <viveksh09876@gmail.com>
2025-12-19 00:33:21 +0530

    feat(host): bootstrap authentication from jwt on app load

--- host-app/src/App.tsx
+++ host-app/src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { useAppDispatch } from "./store/hooks";
+import { bootstrapAuth } from "./utils/authBootstrap";
 
 function App() {
-  return <AppRoutes />
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    bootstrapAuth(dispatch);
+  }, [dispatch]);
+
+  return <AppRoutes />;
 }
 
 export default App;
